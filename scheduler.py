f05df3e10d80923a059bbfe5844209c4640cf5bf
LeiMai <mike6453andy3181@gmail.com>
2025-07-23 23:03:25 +0800

    test: scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,9 @@
-import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from app.fetcher.daily_asset_snapshot import daily_asset_snapshot
 from app.fetcher.fetch_etf_daily import fetch_and_save as fetch_etf_daily
 from app.push.push_utils import push_flex_to_targets
-from app.push.flex_utils import get_full_flex_carousel  # 你要建立這支組合 function
+from app.push.flex_utils import get_full_flex_carousel 
 
 def fetch_all_data():
     fetch_etf_daily("BTC", days=5)
@@ -21,10 +20,10 @@ def main():
     sched = BlockingScheduler(timezone="Asia/Taipei")
 
     # 13:50 抓取資料
-    sched.add_job(fetch_all_data, CronTrigger(hour=22, minute=46))
+    sched.add_job(fetch_all_data, CronTrigger(hour=23, minute=8))
     print('爬取數據中')
     # 14:00 推播
-    sched.add_job(push_all_reports, CronTrigger(hour=22, minute=49))
+    sched.add_job(push_all_reports, CronTrigger(hour=23, minute=12))
     print('推播完成')
 
     print("🔔 Scheduler started, press Ctrl+C to quit")
