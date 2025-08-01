61f6ec8201b9d9ffbf3094f67f1bca8814f8ecc7
Le1Ma1 <mike6453andy3181@gmail.com>
2025-08-01 16:45:38 +0800

    save

--- app/push/push_btc_holder.py
+++ app/push/push_btc_holder.py
@@ -1,12 +1,12 @@
 from app.db import query_btc_holder_distribution
 from app.plot_chart_btc_holder import plot_btc_holder_pie
-from app.push.push_etf_chart import upload_imgbb
+from app.push.push_etf_chart import upload_to_r2
 
 def get_flex_bubble_btc_holder(days=1):
     df_hist = query_btc_holder_distribution(days=days)
     today = df_hist['date'].max().strftime("%Y-%m-%d")
     df_today = df_hist[df_hist['date'] == df_hist['date'].max()]
-    img_pie = upload_imgbb(plot_btc_holder_pie(df_today, today))
+    img_pie = upload_to_r2(plot_btc_holder_pie(df_today, today))
 
     bubble = {
         "type": "bubble",
